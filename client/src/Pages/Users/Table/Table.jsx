84de8afb6ec6a324b02d2378dbda3d0616ee5db0
naumanch969 <naumanch969@gmail.com>
2023-08-03 10:51:19 +0500

    create lead page added

--- client/src/Pages/Users/Table/Table.jsx
+++ client/src/Pages/Users/Table/Table.jsx
@@ -3,18 +3,32 @@ import { Avatar, AvatarGroup, Tooltip } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
 import { DeleteOutline, Edit, EditAttributes, EditOutlined, More, MoreOutlined, PanoramaFishEye, ViewAgenda } from '@mui/icons-material';
 import { Eye } from 'react-bootstrap-icons';
+import EditModal from './EditModal';
+import React from 'react'
+import DeleteModal from './DeleteModal';
 
 
 const Table = ({ users }) => {
-    console.log(users)
+
+    const [changePassword, setChangePassword] = useState('')
+    const [openEditModal, setOpenEditModal] = useState(false)
+    const [openDeleteModal, setOpenDeleteModal] = useState(false)
+
     const columns = [
         {
-            field: 'image', headerName: 'Photo', width: 100, editable: true, renderCell: (params) => (
+            field: 'image', headerName: 'Photo', width: 100, renderCell: (params) => (
                 <img src={params.row.image} alt="" className='w-[2rem] h-[2rem] rounded-full object-cover ' />
             )
         },
-        { field: 'userName', headerName: 'User Name', width: 150, editable: true, },
+        { field: 'userName', headerName: 'User Name', width: 150, },
         { field: 'password', headerName: 'Password', width: 150 },
+        {
+            field: 'changePassword', headerName: 'Change Password', width: 150, renderCell: (params) => (
+                <React.Fragment>
+                    <input type="password" value={changePassword} onChange={(e) => setChangePassword(e.target.value)} />
+                </React.Fragment>
+            )
+        },
         { field: 'email', headerName: 'Email', width: 150 },
         { field: 'officialNumber', headerName: 'Office No.', width: 150 },
         { field: 'phone', headerName: 'Mobile Number', width: 150 },
@@ -26,7 +40,9 @@ const Table = ({ users }) => {
                 return (
                     <span className={`
                 ${activeStatus ? 'border-green-500 text-green-500' : 'border-red-500 text-red-500'}
-                 border-[1px] rounded-[6px] px-[4px] py-[2px]  `} >{activeStatus ? 'Active' : 'Inactive'}</span>
+                border-[1px] rounded-[6px] px-[4px] py-[2px]
+                `}
+                    >{activeStatus ? 'Active' : 'Inactive'}</span>
                 )
             }
         },
@@ -34,7 +50,7 @@ const Table = ({ users }) => {
             field: "action", headerName: "Action", width: 200, renderCell: (params) => (
                 <div className='flex gap-[4px] ' >
                     <Tooltip placement='top' title='Edit' >
-                        <button className='cursor-pointer ' ><EditOutlined /></button>
+                        <button onClick={() => setOpenEditModal(true)} className='cursor-pointer ' ><EditOutlined /></button>
                     </Tooltip>
                     <Tooltip placement='top' title='Delete' >
                         <button className='cursor-pointer ' ><DeleteOutline /></button>
@@ -47,6 +63,8 @@ const Table = ({ users }) => {
     return (
         <div className='w-[61rem] h-auto overflow-x-scroll '>
 
+            <EditModal open={openEditModal} setOpen={setOpenEditModal} />
+            <DeleteModal open={openDeleteModal} setOpen={setOpenDeleteModal} />
 
             <DataGrid
                 rows={users}
