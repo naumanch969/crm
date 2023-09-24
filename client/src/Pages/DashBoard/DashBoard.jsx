ea7b6b841a76790858e124e8d6b9f8cda44d5673
Hamza Zulfiqar <hamzazulfiqar123123@gmail.com>
2023-09-24 19:53:46 +0500

    Added Calendar

--- client/src/Pages/DashBoard/DashBoard.jsx
+++ client/src/Pages/DashBoard/DashBoard.jsx
@@ -5,19 +5,19 @@ import IncomeAndExpense from "./IncomeAndExpense";
 import LeadsStat from "./LeadsStat";
 import ProjectsStats from "./ProjectsStats";
 import Messages from "./Messages";
+import MeetingCalendar from "./MeetingCalendar/MeetingCalendar";
 
 function DashBoard() {
-
-  
   return (
     <div className="w-full h-auto">
       <PaymentStats />
-
+      <div className="p-4 bg-white my-4 rounded-md">
+        <MeetingCalendar />
+      </div>
       <Box className="flex-none mt-5 lg:flex">
         <IncomeAndExpense />
         <LeadsStat />
       </Box>
-
     </div>
   );
 }
